c3f3fb8b8713a5f79d4529be7a7d6143bf33f726
Daniela Muñoz <163449783+LauraDanielaa@users.noreply.github.com>
2024-10-14 12:52:03 -0500

    Creating the panel buttons functionalities in the Script

--- Show Help for GUI elements/GUI/main.gd
+++ Show Help for GUI elements/GUI/main.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@onready var panel_principal = $PrincipalPanelLayer
 
 func _on_help_button_pressed() -> void:
-	pass
-
-
-func _on_configuration_button_pressed() -> void:
-	pass # Replace with function body.
+	if not panel_principal.visible:
+		panel_principal.show()
+	else:
+		panel_principal.hide()	
+		
+	
+func _on_exit_button_pressed() -> void:
+	panel_principal.hide()
+	
