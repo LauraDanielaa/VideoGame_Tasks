3d98e7ea4eeffccc3381228b74cbeb6a22dde460
Daniela Muñoz <163449783+LauraDanielaa@users.noreply.github.com>
2024-11-05 15:43:34 -0500

    Add tweens to buttons
    Add tweens to scale buttons when they are pressed

--- second installment - Help panel GUI/GUI/button.gd
+++ second installment - Help panel GUI/GUI/button.gd
@@ -1,16 +1,19 @@
 extends Button
 
-@export var right_panel_text : RightPanelText #Instanciacion del recurso 
+@export var right_panel_text : RightPanelText # Instanciación del recurso
 @onready var rich_text_label = $"../../../VBoxContainer/RightPanel/CenterContainer/RichTextLabel"
-@onready var left_panel_button_audio =  $LeftPanelAudio
+@onready var left_panel_button_audio = $LeftPanelAudio
 
 func _ready():
-	pressed.connect(_display_ayuda)
-
-
-func _display_ayuda() -> void:
-	rich_text_label.text = right_panel_text.texto_ayuda
-	
+	pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
-	left_panel_button_audio.playing = true 
+	left_panel_button_audio.playing = true
+	
+	var tween = self.create_tween()  
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.1)
+	tween.tween_property(self, "scale", scale, 0.1)\
+		.set_trans(Tween.TRANS_BOUNCE)\
+		.set_ease(Tween.EASE_OUT)
+
+	rich_text_label.text = right_panel_text.texto_ayuda
--- second installment - Help panel GUI/GUI/main.gd
+++ second installment - Help panel GUI/GUI/main.gd
@@ -9,6 +9,13 @@ func _ready() -> void:
 	
 
 func _on_help_button_pressed() -> void:
+	
+	var tween = self.create_tween()  
+	tween.tween_property($HelpButtonLayer/HBoxContainer/help_button, "scale", Vector2(1.1, 1.1), 0.1)
+	tween.tween_property($HelpButtonLayer/HBoxContainer/help_button, "scale", scale, 0.1)\
+		.set_trans(Tween.TRANS_BOUNCE)\
+		.set_ease(Tween.EASE_OUT)
+	
 	help_button_audio.playing = true 
 	if not panel_principal.visible:
 		panel_principal.show()
@@ -17,6 +24,6 @@ func _on_help_button_pressed() -> void:
 		
 		
 func _on_exit_button_pressed() -> void:
-	exit_button_audio.playing = true 
+	exit_button_audio.playing = true		
 	panel_principal.hide()
 	
